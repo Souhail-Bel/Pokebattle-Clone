ae93b7b4405d91a6cee83a771b690c5968c50fc2
Talel Zighni <talelzigni24@gmail.com>
2026-05-03 14:57:57 +0100

    added todos

--- src/main/java/com/example/entity/Pokemon.java
+++ src/main/java/com/example/entity/Pokemon.java
@@ -1,10 +1,14 @@
 public class Pokemon{
+    //this is suffient for a pokemon
     String name;
     int attack;
     int defense;
+    public Pokemon(String name){
+	//TODO add fetchin from a db
+    }
     public Pokemon( String name, int attack, int defense){
 	this.name=name;
 	this.attack=attack;
 	this.defense=defense;
-    }
+    } 
 }
