f774880ef4f7fd696353c1b7a61a4069a3bca271
Talel Zighni <talelzigni24@gmail.com>
2026-05-03 14:50:00 +0100

    added the first class

---
+++ src/main/java/com/example/entity/Pokemon.java
@@ -0,0 +1,10 @@
+public class Pokemon{
+    String name;
+    int attack;
+    int defense;
+    public Pokemon( String name, int attack, int defense){
+	this.name=name;
+	this.attack=attack;
+	this.defense=defense;
+    }
+}
